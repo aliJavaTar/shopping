6b9ec68237fba83e11f4f4a40b36a75d5c53a590
aliJavaTar <ali77erfagh99@gmail.com>
2021-08-18 20:28:05 +0430

    repository clean code

--- src/main/java/entity/Cart.java
+++ src/main/java/entity/Cart.java
@@ -1,2 +1,44 @@
-package entity.base;public class Cart {
+package entity;
+
+import entity.base.BaseEntity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Table(name = "carts")
+public class Cart extends BaseEntity<Long> implements Serializable {
+    @Column(name = "capacity")
+    int capacity = 5;
+
+    @OneToMany(mappedBy = "cart",cascade= CascadeType.ALL)
+    private List<CartItem> cartItems;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private User user;
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public List<CartItem> getCartItems() {
+        return cartItems;
+    }
+
+    public void setCartItems(List<CartItem> cartItems) {
+        this.cartItems = cartItems;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
